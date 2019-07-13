9ac6da547ac508059475151c03180c718bfd989c
Alicia Tamboli <atamboli4718@gmail.com>
2019-07-13 19:37:24 -0400

    added home page and beginging of js file

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+/*
+10 Questions:
+1-5, 1 strongly disagree & 5 strongly agree
+- You'll find me home enjoying the quiet on the weekends.
+- I believe in love at first sight.
+- Working out is an important part of my routine.
+- I prefer being in the outdoors for my leisure activities.
+- I love to sleep in the weekends.
+- My work is a very important part of my life.
+- Music helps me to relax.
+- I prefer making a homecooked meal to going out.
+- When I go out, I am the life of the party.
+- I prefer going to tried and true vacation locations over risking it on a new place.
+*/
+
+
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var friends = [
+    {
+    "name":"Amber",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ]
+    }
+  ];
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening");
+  });
